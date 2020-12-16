3f0780382484d28baae0c7daae1cdceda8a88300
juanbaltasar <martinjuanbaltasar@gmail.com>
2020-12-15 21:06:59 -0300

    Terminado

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Codificador></Codificador>
+    <p>1-b, 2-b, 3-c, 4-b, 5-c</p>
+  </div>
+</template>
+
+<script>
+import Codificador from './components/Codificador.vue'
+export default {
+  name: 'App',
+  components: {Codificador},
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Codificador.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <textarea name="" v-model="textoACodificar" placeholder="Inserte el texto a codificar!"></textarea>
+    <h1>{{TextoCodificadoAMayuscula}}</h1>
+  </div>
+</template>
+
+<script>
+
+  export default  {
+    name: 'Codificador',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        textoACodificar: ''
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      TextoCodificadoAMayuscula(){
+        const TextoEnArray = this.textoACodificar.toLowerCase().split('')
+            let TextoCodificado = ''
+            for (const letra of TextoEnArray) {
+                if (letra === 'a') {
+                    TextoCodificado += 'u'
+                }
+                else if (letra === 'e') {
+                    TextoCodificado += 'o'
+                }
+                else if (letra === 'o') {
+                    TextoCodificado += 'e'
+                }
+                else if (letra === 'u') {
+                    TextoCodificado += 'a'
+                }
+                else{
+                    TextoCodificado += letra
+                }
+            }
+            return TextoCodificado.toUpperCase()
+    }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+</style>
